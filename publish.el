f533c9ac152713db853176720e142bc74074280e
Lincoln Clarete <lincoln@clarete.li>
2020-08-08 23:46:49 -0400

    Some CSS and HTML to display the autogenerated API page using blorg

---
+++ publish.el
@@ -0,0 +1,81 @@
+;;; publish.el --- Generate HTML Documentation; -*- lexical-binding: t -*-
+;;
+;; Author: Lincoln Clarete <lincoln@clarete.li>
+;;
+;; Copyright (C) 2020  Lincoln Clarete
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;;; Commentary:
+;;
+;; Genenrate static websites off of Org Mode sources.
+;;
+;;; Code:
+
+;; Initialize packaging & install `use-package' to download external
+;; dependencies.
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+;; Install `use-package' enabled dependencies.  We need `jinja2-mode'
+;; because `web-mode' doesn't seem to play well with `htmlize' on
+;; Org-Mode blocks.
+(use-package jinja2-mode :config :ensure t)
+;; Org-Mode depends on `htmlize' in order to produce HTML off Org-Mode
+;; blocks with syntax highlighting.
+(use-package htmlize :config :ensure t)
+;; Use the latest version of templatel by importing it from the
+;; current directory
+(add-to-list 'load-path ".")
+;; Use the latest version of blorg, by either using a cloned version
+;; or cloning it from scratch if we're running on the CI. Maybe we'll
+;; move this require to use `use-package' after blorg is available via
+;; melpa as well.
+(if (file-directory-p "~/src/github.com/clarete/blorg")
+    (add-to-list 'load-path "~/src/github.com/clarete/blorg")
+  (shell-command
+   "cd /tmp &&       # go somewhere we can't break things
+    rm -rf blorg &&  # clean up previous runs and get it
+    git clone https://github.com/clarete/blorg")
+  (add-to-list 'load-path "/tmp/blorg"))
+
+;; --- Actual HTML generation setup ---
+
+(require 'blorg)
+
+;; Tells `htmlize' library to output HTML with css classes instead of
+;; directly formatting the output.
+(setq org-html-htmlize-output-type 'css)
+
+;; Generate Index Page
+(blorg-cli
+ :input-pattern "docs/src/index.org$"
+ :template "index.html"
+ :output "docs/index.html")
+
+;; Generate API Reference
+(blorg-cli
+ :input-source (blorg-input-source-autodoc-sections
+                `(("Render template strings" . "^templatel-render")
+                  ("Template environments" . "^templatel-env")
+                  ("Filters" . "^templatel-filter")
+                  ("Exceptions" . ,(concat "templatel-" (regexp-opt '("syntax-error" "runtime-error" "backtracking"))))))
+ :template "autodoc.html"
+ :output "docs/api.html")
+
+(provide 'publish)
+;;; publish.el ends here
