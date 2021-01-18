1a7784e5ec9a5e43adae56674aa63e8b3cf7a2cd
Lincoln de Sousa <lincoln@clarete.li>
2021-01-18 12:22:56 -0500

    New Version: 0.1.4

--- templatel.el
+++ templatel.el
@@ -2,7 +2,7 @@
 ;;
 ;; Author: Lincoln Clarete <lincoln@clarete.li>
 ;; URL: https://clarete.li/templatel
-;; Version: 0.1.3
+;; Version: 0.1.4
 ;; Package-Requires: ((emacs "25.1"))
 ;;
 ;; Copyright (C) 2020  Lincoln Clarete
